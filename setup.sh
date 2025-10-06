c9b3c01f7d3d11c50370ce9f592405c44433f95e
sushilmiitb <sushilm.iitb@gmail.com>
2025-10-06 10:43:48 +0530

    Made container to bind to port 8081 on the host

--- setup.sh
+++ setup.sh
@@ -51,7 +51,7 @@ server {
     ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
 
     location / {
-        proxy_pass http://127.0.0.1:8080;
+        proxy_pass http://127.0.0.1:8081;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -67,7 +67,7 @@ server {
     listen 8080;
     server_name $DOMAIN;
     location / {
-        proxy_pass http://127.0.0.1:8080;
+        proxy_pass http://127.0.0.1:8081;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
